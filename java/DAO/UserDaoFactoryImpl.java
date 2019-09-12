d054298a9a0b2d4a53408a12f9e9a6ff7ac6c183
Daemod <Daemods@gmail.com>
2019-09-12 15:47:18 +0300

    PreProject 3

--- java/DAO/UserDaoFactoryImpl.java
+++ java/DAO/UserDaoFactoryImpl.java
@@ -1,41 +1,18 @@
 package DAO;
 
 import util.DBHelper;
+import util.PropertyReader;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 public class UserDaoFactoryImpl implements UserDaoFactory {
-    private enum ConType {
-        JDBC, HIBERNATE
-    }
+    private PropertyReader propertyReader;
 
     private static UserDaoFactory factory;
     private static DBHelper dbHelper = DBHelper.getInstance();
-    private static ConType typeConnection;
 
 
     private UserDaoFactoryImpl() {
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-
-        Properties properties = new Properties();
-
-        try {
-            InputStream inputStream = loader.getResourceAsStream("config.properties");
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        switch (properties.getProperty("connector")) {
-            case "JDBC":
-                typeConnection = ConType.JDBC;
-                break;
-            case "Hibernate":
-                typeConnection = ConType.HIBERNATE;
-                break;
-        }
+        propertyReader = new PropertyReader();
     }
 
 
@@ -47,13 +24,10 @@ public class UserDaoFactoryImpl implements UserDaoFactory {
     }
 
     public UserDao getUserDao() {
-        if (typeConnection == ConType.JDBC) {
-            return new UserDaoJDBCImpl(dbHelper.getConnection());
-        } else if (typeConnection == ConType.HIBERNATE) {
-            return new UserDaoHibernateImpl(dbHelper.getConfiguration());
-        } else {
-            System.out.println("Error config.properties in field \"connector\"");
-            return null;
+        switch (propertyReader.getProperty()){
+            case "JDBC": return new UserDaoJDBCImpl(dbHelper.getConnection());
+            case "Hibernate": return new UserDaoHibernateImpl(dbHelper.getConfiguration());
+            default: System.out.println("Configuration error"); return null;
         }
     }
 
---
+++ java/util/PropertyReader.java
@@ -0,0 +1,28 @@
+package util;
+
+import DAO.UserDaoFactoryImpl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+public class PropertyReader {
+    private String connector;
+    public PropertyReader(){
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties properties = new Properties();
+        try {
+            InputStream inputStream = loader.getResourceAsStream("config.properties");
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        connector = properties.getProperty("connector");
+    }
+
+    public String getProperty(){
+        return connector;
+    }
+}
