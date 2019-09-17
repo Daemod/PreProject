7544018f09289643caa8b71ae045e8b0b7b8c9e5
Daemod <Daemods@gmail.com>
2019-09-17 16:17:47 +0300

    PreProject 4 Annotation @WebServlet and @WebFilter

--- java/servlets/DeleteUserServlet.java
+++ java/servlets/DeleteUserServlet.java
@@ -3,12 +3,14 @@ package servlets;
 import service.UserService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 
+@WebServlet("/deleteUser")
 public class DeleteUserServlet extends HttpServlet {
     private UserService service = UserService.getInstance();
 
--- java/servlets/FilterServlet.java
+++ java/servlets/FilterServlet.java
@@ -4,34 +4,32 @@ import model.User;
 import service.UserService;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 
+@WebFilter("/*")
 public class FilterServlet implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        String path = request.getContextPath();
 
-        if (!path.contains("admin")) filterChain.doFilter(servletRequest, servletResponse);
-        else {
-            String login = request.getParameter("login");
-            String password = request.getParameter("password");
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
 
-            User user = UserService.getInstance().getUserByName(login);
-            if (user == null) request.getRequestDispatcher("/index.jsp").forward(servletRequest, servletResponse);
-            if (user.getRole().equals("admin") && user.getPassword() == password.hashCode()) {
-                filterChain.doFilter(servletRequest, servletResponse);
-            }
-
+        User user = UserService.getInstance().getUserByName(login);
+        if (user == null) request.getRequestDispatcher("/index.jsp").forward(servletRequest, servletResponse);
+        if (user.getRole().equals("admin") && user.getPassword() == password.hashCode()) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            request.getRequestDispatcher("/index.jsp").forward(servletRequest, servletResponse);
         }
+
+
     }
 
     @Override
