4543873aff1bb4917a82a6894bdb105ce78e79d4
Daemod <Daemods@gmail.com>
2019-09-20 15:32:36 +0300

    PreProject 4 Two filters reworked

--- java/servlets/AdminFilter.java
+++ java/servlets/AdminFilter.java
@@ -5,7 +5,6 @@ import model.User;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
